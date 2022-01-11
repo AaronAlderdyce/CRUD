14c27ca3ec5b43d4812e31e5aa242ba91c251d07
Aaron Alderdyce <aaron_alderdyce@hotmail.com>
2022-01-11 09:12:59 -0800

    setting up more of the form's JS

--- script.js
+++ script.js
@@ -14,9 +14,14 @@ function onFormSubmit() {
 function readFormData() {
     var formData = {};
     formData["firstName"] = document.getElementById("firstName").value;
-    formData["empCode"] = document.getElementById("empCode").value;
-    formData["salary"] = document.getElementById("salary").value;
+    formData["middleName"] = document.getElementById("middleName").value;
+    formData["lastName"] = document.getElementById("lastName").value;
+    formData["email"] = document.getElementById("email").value;
+    formData["address"] = document.getElementById("address").value;
+    formData["dob"] = document.getElementById("dob").value;
     formData["city"] = document.getElementById("city").value;
+    formData["state"] = document.getElementById("state").value;
+    formData["zip"] = document.getElementById("zip").value;
     return formData;
 }
 
@@ -26,21 +31,36 @@ function insertNewRecord(data) {
     cell1 = newRow.insertCell(0);
     cell1.innerHTML = data.firstName;
     cell2 = newRow.insertCell(1);
-    cell2.innerHTML = data.empCode;
+    cell2.innerHTML = data.middleName;
     cell3 = newRow.insertCell(2);
-    cell3.innerHTML = data.salary;
+    cell3.innerHTML = data.lastName;
     cell4 = newRow.insertCell(3);
-    cell4.innerHTML = data.city;
-    cell4 = newRow.insertCell(4);
-    cell4.innerHTML = `<a onClick="onEdit(this)">Edit</a>
+    cell4.innerHTML = data.email;
+    cell5 = newRow.insertCell(4);
+    cell5.innerHTML = data.address;
+    cell6 = newRow.insertCell(5);
+    cell6.innerHTML = data.dob;
+    cell7 = newRow.insertCell(6);
+    cell7.innerHTML = data.city;
+    cell8 = newRow.insertCell(7);
+    cell8.innerHTML = data.state;
+    cell9 = newRow.insertCell(8);
+    cell9.innerHTML = data.zip;
+    cell10 = newRow.insertCell(9);
+    cell10.innerHTML = `<a onClick="onEdit(this)">Edit</a>
                        <a onClick="onDelete(this)">Delete</a>`;
 }
 
 function resetForm() {
     document.getElementById("firstName").value = "";
-    document.getElementById("empCode").value = "";
-    document.getElementById("salary").value = "";
+    document.getElementById("middleName").value = "";
+    document.getElementById("lastName").value = "";
+    document.getElementById("email").value = "";
+    document.getElementById("address").value = "";
+    document.getElementById("dob").value = "";
     document.getElementById("city").value = "";
+    document.getElementById("state").value = "";
+    document.getElementById("zip").value = "";
     selectedRow = null;
 }
 
